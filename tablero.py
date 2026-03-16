9f4f161f8aaee8ce697c4a5885b9b7fbc10455e5
Casal <javierlolcasal@gmail.com>
2026-03-16 14:33:31 +0100

    tablero con casillas, disitntos tipos de naves creadas, y naves colocadas en el casillero

--- tablero.py
+++ tablero.py
@@ -1,8 +1,8 @@
-
+from nave import Nave
 
 class Tablero:
 
-    def __init__(self):
+    def __init__(self, tamano = 10):
         self.nave = None
         self.tamano = 10
 
@@ -10,10 +10,38 @@ class Tablero:
         self.TOCADO = 1
         self.HUNDIDO = 2
 
+        por1 = Nave("Enterprise", "portaaviones", 5)
+
+        fra1 = Nave("Bismarck", "fragata", 3)
+        fra2 = Nave("Prince of Wales", "fragata", 3)
+        fra3 = Nave("Graf Spee", "fragata", 3)
+
+        sub1 = Nave("U-47", "submarino", 1)
+        sub2 = Nave("U-96", "submarino", 1)
+        sub3 = Nave("U-505", "submarino", 1)
+        sub4 = Nave("U-534", "submarino", 1)
+
+
+        self.casillero = [
+            [None, None, None, None, None, None, None, None, None, None],
+            [None, por1, por1, por1, por1, por1, None, None, None, None],
+            [None, None, None, None, None, None, None, None, None, None],
+            [None, None, None, fra1, None, None, None, None, None, None],
+            [None, None, None, fra1, None, None, sub1, None, None, None],
+            [None, None, None, fra1, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None, None, None],
+            [None, fra2, fra2, fra2, None, None, sub3, None, None, None],
+            [None, None, None, None, None, None, None, None, None, None],
+            [None, fra3, fra3, fra3, None, sub4, None, None, None, sub2]
+        ]
+
     def colocar_nave(self, nave):
-        self.nave = nave
+        pass
 
     def comprobar_impacto(self, x, y):
-        if self.nave is None:
-            return self.AWA
-        return self.nave.recibir_disparo()
+        print(f"[LOG] estoy en tablero comprobando impacto ({x}, {y})")
+        print(f"[LOG] casillero[{x}][{y}] = {self.casillero[x][y]}")
+        if self.casillero[x][y] is None:
+            print("[LOG] Awa")
+        print(f"[LOG] {self.casillero[x][y].nombre} Tocado")
+        return self.AWA
